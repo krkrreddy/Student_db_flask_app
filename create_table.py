9fd2c57bc78e271b2819f4c35a657bc70141cf15
ravikanth <ravi1krkr@gmail.com>
2020-04-17 17:23:05 +0530

    Add files via upload

--- create_table.py
+++ create_table.py
@@ -12,10 +12,10 @@ cursor = conn.cursor()
 
 # MUST BE INTEGER
 # This is the only place where int vs INTEGER matters—in auto-incrementing columns
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE users (id INTEGER PRIMARY KEY, username nvarchar(10), password nvarchar(10)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS student (student text PRIMARY KEY, roll_number INTEGER)"
+create_table = "CREATE TABLE student (student text PRIMARY KEY, roll_number INTEGER)"
 cursor.execute(create_table)
 
 conn.commit()
